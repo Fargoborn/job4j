063335de73ca39bd6ba2ad6dce35c67916742685
dbarshhevskijj <dealba1@yandex.ru>
2019-03-20 15:00:04 +0300

    1.3. JavaDoc. Code convention[#123020]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,12 +1,21 @@
 ﻿package ru.job4j.calculate;
 
+/**
+* Calculate.
+*
+* @author Denis Barshchevsky (dealba1@yandex.ru)
+*/
 public class Calculate{
 	public String echo(String value){
 	return String.format("%s %s %s", value, value, value);
 	}
 
+/**
+* Main.
+*@param calc - object.
+*/
   public static void main(String[] args){
   Calculate calc = new Calculate();
-    System.out.println(calc.echo("эх"));
+    System.out.println(calc.echo("aah"));
   }
 }
