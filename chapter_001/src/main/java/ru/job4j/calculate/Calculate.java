3f795cb7d56d129e32ce014d39efe2f0bc8c78c9
Fargoborn <dealba1@yandex.ru>
2019-03-20 23:54:10 +0300

    1.5. Checkstyle[#123022]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -5,12 +5,12 @@ package ru.job4j.calculate;
 *
 * @author Denis Barshchevsky (dealba1@yandex.ru)
 */
-public class Calculate{
+public class Calculate {
 /**
 * Main.
 *@param args - args.
 */
-  public static void main(String[] args){
+  public static void main(String[] args) {
   Calculate calc = new Calculate();
     System.out.println(calc.echo("aah"));
   }
