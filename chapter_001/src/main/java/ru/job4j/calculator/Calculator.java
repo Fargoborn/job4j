ce10134b2c4ec5993aecc8ab30bb8975e1eab550
Fargoborn <dealba1@yandex.ru>
2019-03-21 22:27:47 +0300

    3.1. Элементарный калькулятор.[#123010] Добавление JavaDoc

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -6,15 +6,40 @@ package ru.job4j.calculator;
 * @author Denis Barshchevsky (dealba1@yandex.ru)
 */
 public class Calculator {
+
+    /**
+     * Сложение.
+     * @param first Первое слогаемое.
+     * @param second Второе слогаемое.
+     * @return Сумма.
+     */
     public double add(double first, double second) {
     return first + second;
     }
+    /**
+     * Вычитание.
+     * @param first Уменьшаемое.
+     * @param second Вычитаемое.
+     * @return Разность.
+     */
     public double minus(double first, double second) {
     return first - second;
     }
+    /**
+     * Деление.
+     * @param first Делимое.
+     * @param second Делитель.
+     * @return Часное.
+     */
     public double division(double first, double second) {
         return first / second;
     }
+    /**
+     * Умножение.
+     * @param first Первый множитель.
+     * @param second Второй множитель.
+     * @return Произведение.
+     */
     public double multiplication(double first, double second) {
         return first * second;
     }
--- chapter_001/src/main/java/ru/job4j/calculator/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Package for calculator task.
-*
-* @author Denis Barshchevsky (dealba1@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j.calculator;
