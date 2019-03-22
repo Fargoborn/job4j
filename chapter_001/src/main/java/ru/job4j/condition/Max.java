583bf363851d4baa93c3190ef40528133a6c4d8e
dbarshhevskijj <dealba1@yandex.ru>
2019-03-22 15:27:16 +0300

    4.2. Максимум из двух чисел[#122980]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+/**
+ * Point.
+ *
+ * @author Denis Barshchevsky (dealba1@yandex.ru)
+ */
+public class Max {
+    /**
+     * Максимальное из двух чисел.
+     * @param left первое число.
+     * @param right второе число.
+     * @return Максимальное.
+     */
+    public int max(int left, int right) {
+        return left > right ? left:right;
+    }
+}
