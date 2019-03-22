cbb12c5c29b0813f9bfe83a4e7b78742e2839e0f
dbarshhevskijj <dealba1@yandex.ru>
2019-03-22 11:06:11 +0300

    3.4. Расстояние между точками в системе координат[#123011] поправки в JavaDoc

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -11,7 +11,7 @@ public class Point {
      * @param x1 абсцисса точки 1.
      * @param x2 абсцисса точки 2.
      * @param y1 ордината точки 1.
-     * @param y2 ордината точки 1.
+     * @param y2 ордината точки 2.
      * @return Расстояние.
      */
     public double distance(int x1, int y1, int x2, int y2) {
