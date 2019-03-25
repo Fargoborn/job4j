ee523c270986348ebb9e491c6e8c18f1aef5dbd7
dbarshhevskijj <dealba1@yandex.ru>
2019-03-25 12:47:35 +0300

    5.3. Построить шахматную доску в псевдографике.[#122992]

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -5,15 +5,22 @@ package ru.job4j.loop;
  *
  * @author Denis Barshchevsky (dealba1@yandex.ru)
  */
+
 public class Board {
+  /**
+   * Создаем шахматную доску.
+   * @param width ширина.
+   * @param height высота.
+   * @return Шахматная доска.
+   */
   public String paint(int width, int height) {
     StringBuilder screen = new StringBuilder();
     String ln = System.lineSeparator();
-    for ( ) {
-      for ( ) {
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
         // условие проверки, что писать пробел или X
         // Выше в задании мы определили закономерность, когда нужно проставлять X
-        if () {
+        if ((i + j) % 2 == 0) {
           screen.append("X");
         } else {
           screen.append(" ");
--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -11,13 +11,21 @@ import static org.junit.Assert.assertThat;
  */
 public class BoardTest {
   @Test
-  public void when3x3() {
+  public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
     Board board = new Board();
-    String rsl = board.paint(3, 3);
-    String ln = System.lineSeparator();
-    assertThat(rsl, is(
-            String.format("X X%s X %sX X%s", ln, ln, ln)
-            )
-    );
+    String result = board.paint(3, 3);
+    final String line = System.getProperty("line.separator");
+    String expected = String.format("X X%s X %sX X%s", line, line, line);
+    assertThat(result, is(expected));
+  }
+
+  @Test
+  public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+    //напишите здесь тест, проверяющий формирование доски 5 на 4.
+    Board board = new Board();
+    String result = board.paint(5, 4);
+    final String line = System.getProperty("line.separator");
+    String expected = String.format("X X X%s X X %sX X X%s X X %s", line, line, line, line);
+    assertThat(result, is(expected));
   }
 }
