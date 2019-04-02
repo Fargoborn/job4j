1da3c03f3c510b0f3f77de5d645ed005637986c7
Fargoborn <dealba1@yandex.ru>
2019-04-03 01:19:21 +0300

    1. Рефакторинг - Расстояние между точками.[#123095]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,20 +1,37 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 /**
  * Point.
  *
  * @author Denis Barshchevsky (dealba1@yandex.ru)
  */
 public class Point {
+
     /**
-     * Расстояние между точками.
-     * @param x1 абсцисса точки 1.
-     * @param x2 абсцисса точки 2.
-     * @param y1 ордината точки 1.
-     * @param y2 ордината точки 2.
-     * @return Расстояние.
+     * Это поле объекта. Оно доступно только конкретному объекту.
      */
+    private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int y;
+
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     * @param first координата x
+     * @param second координата y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
     public double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    }
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -15,8 +15,16 @@ import static org.junit.Assert.assertThat;
 public class PointTest {
     @Test
     public void whenZeroAndTenThenTen() {
-        Point point = new Point();
+        Point point = new Point(0, 0);
         double result = point.distance(0, 0, 0, 10);
         assertThat(result, is(10D));
+    }
+
+    @Test
+    public void whenShowInfo() {
+        Point first = new Point(1, 1);
+        first.info();
+        Point second = new Point(2, 2);
+        second.info();
     }
 }
