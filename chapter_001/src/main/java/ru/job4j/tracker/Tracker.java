b57ba3c86fb9db705a91e1b1a85354f8e8d0a6f1
dbarshhevskijj <dealba1@yandex.ru>
2019-04-05 17:42:30 +0300

    2. Реализовать класс Tracker[#123084] тесты ОК!

--- chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
@@ -18,6 +18,17 @@ public class Tracker {
     private int position = 0;
 
     /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        String id = String.valueOf(((int) (System.currentTimeMillis() + Math.random())));
+        //System.out.println(id);
+        return id;
+    }
+
+    /**
      * Метод реализаущий добавление заявки в хранилище
      * @param item новая заявка
      */
@@ -28,13 +39,17 @@ public class Tracker {
     }
 
     /**
-     * Метод генерирует уникальный ключ для заявки.
-     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
-     * @return Уникальный ключ.
+     * Метод реализаущий получение заявки по id
+     * @param id идентификатор заявки
      */
-    private String generateId() {
-        String id = String.valueOf(((int) (System.currentTimeMillis() + Math.random())));
-        return id;
+    public Item findById(String id) {
+        Item item = null;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && items[i].getId().equals(id)) {
+                item = this.items[i];
+            }
+        }
+        return item;
     }
 
     /**
@@ -45,7 +60,7 @@ public class Tracker {
     public boolean replace(String id, Item item) {
         boolean result = false;
         for (int i = 0; i < items.length; i++) {
-            if (items[i].getId().equals(id)) {
+            if (items[i] != null && items[i].getId().equals(id)) {
                 this.items[i] = item;
                 result = true;
             }
@@ -60,16 +75,17 @@ public class Tracker {
     public boolean delete(String id) {
         boolean result = false;
         int uniq = items.length;
-
         for (int out = 0; out < items.length; out++) {
             for (int in = out + 1; in < uniq; in++) {
                 if (items[out].getId().equals(id)) {
+                    items[out] = null;
+                    result = true;
                     items[in] = items[uniq - 1];
                     uniq--;
                 }
             }
         }
-        items = Arrays.copyOf(items, uniq);
+        Arrays.copyOf(items, items.length);
         return result;
     }
 
