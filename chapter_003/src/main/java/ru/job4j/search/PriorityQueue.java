aee312351c4abfafe48dab37e524c62c1df25571
dbarshhevskijj <dealba1@yandex.ru>
2019-05-07 16:56:55 +0300

    2. Очередь с приоритетом на LinkedList[#123045] поправки

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -1,10 +1,11 @@
 package ru.job4j.search;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
+    int priority = 0;
+    String indx = "";// список уровней приоритетов
 
     /**
      * Метод должен вставлять в нужную позицию элемент.
@@ -14,16 +15,37 @@ public class PriorityQueue {
      */
     public void put(Task task) {
         //TODO добавить вставку в связанный список.
-        if (tasks.size() == 0) {
-            tasks.add(0, task);
-        } else {
-            for (int i = 0; i < tasks.size(); i++) {
-                if (task.getPriority() <= tasks.get(i).getPriority()) {
-                    tasks.add(i, task);
-                    break;
-                }
+            if (indx.equals("")) {
+                indx = indx + task.getPriority();
+                tasks.add(0, task);
             }
-        }
+            else if (indx.contains(String.valueOf(task.getPriority())) && tasks.size() > 1) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    if (tasks.get(i).getPriority() == task.getPriority()) {
+                        priority = i;
+                    }
+                }
+                tasks.add(priority, task);
+            }
+            else if (!indx.contains(String.valueOf(task.getPriority()))) {
+                 for (int i = 0; i < tasks.size(); i++) {
+                     if (tasks.get(i).getPriority() > task.getPriority()) {
+                         priority = i;
+                         indx = indx + task.getPriority();
+                         break;
+                     }
+                     if (tasks.get(i).getPriority() < task.getPriority() && i == tasks.size() - 1) {
+                                 priority = i + 1;
+                                 indx = indx + task.getPriority();
+                                 break;
+                        } else if (tasks.get(i).getPriority() < task.getPriority() && task.getPriority() < tasks.get(i + 1).getPriority()) {
+                                    priority = i + 1;
+                                    indx = indx + task.getPriority();
+                                    break;
+                     }
+                 }
+                tasks.add(priority, task);
+            }
     }
     public Task take() {
         return this.tasks.poll();
